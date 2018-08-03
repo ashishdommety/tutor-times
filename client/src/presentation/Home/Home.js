e79eee722a52824e23ba1188252d99bc5db82026
unknown <ashishuiux@gmail.com>
2018-08-03 16:44:15 -0700

    add .env.development

--- client/src/presentation/Home/Home.js
+++ client/src/presentation/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Home.css';
+import { GoogleLogin } from 'react-google-login';
+
+const responseGoogle = (response) => console.log(response);
 
 const Home = (props) => {
     return (
@@ -13,8 +16,14 @@ const Home = (props) => {
           A place where tutors and students meet to learn from each other.
         </p>
         <p>{props.status}</p>
-        <button
-          onClick={props.instigateLogin}>Login with Google</button>
+        {/* <button
+          onClick={props.instigateLogin}>Login with Google</button> */}
+        <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+             onFailure={responseGoogle}
+        />
       </div>
     );
 }
