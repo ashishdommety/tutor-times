7e986e17255ba8a1188a55b667a59fc423627121
unknown <ashishuiux@gmail.com>
2018-08-16 15:51:01 -0700

    add responsive base styles for dashboard

--- client/src/presentation/Dashboard/Dashboard.js
+++ client/src/presentation/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Dashboard.css';
 
 const Dashboard = (props) => {
   return(
     <div className="dash">
-      <img src="" alt="thing" />
+      <img src="https://via.placeholder.com/100x100" alt="thing" id="profile_pic" />
       <Link to="/dashboard">Link 1</Link>
       <Link to="/">Link 2</Link>
     </div>
