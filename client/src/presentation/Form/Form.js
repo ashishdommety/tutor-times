bc17a48874fbb23f87ad8c387b9a6389c9eaf4c5
unknown <ashishuiux@gmail.com>
2018-08-23 17:08:28 -0700

    build form outline for tutor

---
+++ client/src/presentation/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = (props) => {
+  return(
+    <div>
+      <input text="text" name="subjects"/>
+      <input text="text" name="motto"/>
+      <input text="text" name="calendarLink"/>
+    </div>
+  )
+};
+
+export default Form;
