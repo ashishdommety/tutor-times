3145b011190779b3a4bfd135bf875440ae3954e6
unknown <ashishuiux@gmail.com>
2018-09-04 14:51:34 -0700

    modularize back-end into 1 controller and 1 route file

---
+++ controllers/index.js
@@ -0,0 +1,94 @@
+/* All of these controller functions are placeholders and are meant to be used for the Tutor View */
+
+module.exports = {
+  nextClass: function(req,res){
+    let data = {
+      "home": {
+        "nextStudent":{
+          "name": "Jane Doe",
+          "date": "August 30th, 2018",
+          "day": "Thursday",
+          "time": "4:30pm",
+          "photoURL": "https://via.placeholder.com/200x200"
+        }
+      }
+    }
+
+    res.send(data);
+  },
+  allStudents: function(req,res){
+    let data = {
+      "students": [
+        {
+          "name": "person",
+          "photoURL": "https://via.placeholder.it/300x300",
+          "subjects":[
+            "physics",
+            "math",
+            "english"
+          ],
+          "nextClass": "August 29th, 2018"
+        },
+        {
+          "name": "human",
+          "photoURL": "https://via.placeholder.it/300x300",
+          "subjects":[
+            "chemisty",
+            "math",
+            "french"
+          ],
+          "nextClass": "August 27th, 2018"
+        },
+        {
+          "name": "alien",
+          "photoURL": "https://via.placeholder.it/300x300",
+          "subjects":[
+            "space science",
+            "math",
+            "history of astroids"
+          ],
+          "nextClass": "September 29th, 2018"
+        }
+      ]
+    }
+
+    res.send(data);
+  },
+  getOneStudent: function(req,res){
+    let id = req.params.id;
+    console.log('the id is: '+ id);
+    let data = {
+        "name": "alien",
+        "photoURL": "https://via.placeholder.it/300x300",
+        "subjects":[
+          "space science",
+          "math",
+          "history of astroids"
+        ],
+        "nextClass": "September 29th, 2018"
+    }
+  res.send(data);
+  },
+  getStudentNotes: function(req,res){
+    let data = {
+      "name": "alien",
+      "photoURL": "https://via.placeholder.it/300x300",
+      "subject": "space science",
+      "notes": [
+        {
+          "date": "01/01/2018",
+          "note": "I learned a lot about space science today - enough already!"
+        },
+        {
+          "date": "01/01/2018",
+          "note": "I learned a lot about space science today too"
+        },
+        {
+          "date": "01/01/2018",
+          "note": "I learned a lot about space science today"
+        },
+      ]
+    }
+    res.send(data);
+  }
+}
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const dummy = require("./dummy.js");
-
-// console.log(dummy);
 
 // Serve static files from the React app (New website)
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,38 +12,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/login", function(req,res){
-  console.log("recieved request for login");
-  res.json({message: 'registered by back-end'});
-  // check in database if user exists
-  // if user doesn't exist, write them in and serve introduction page
-  // if they do exist, respond with their profile object (either tutor or student)
-});
-
-app.get("/test", function(req,res){
-  console.log('call made successfully');
-  res.send(dummy);
-})
-
-app.get("/dashboard/:id", function(req,res){
-  console.log('return all the users data');
-});
-
-app.post("/invite-student", function(req,res){
-  console.log('send an email to a potential student');
-});
-
-app.get("/dashboard/:id/students", function(req,res){
-  console.log("display all of the enlisted students");
-});
-
-app.get("/dashboard/:id/notes", function(req,res){
-  console.log("get all students notes");
-});
-
-app.post("/dashboard/:id/edit-note", function(req,res){
-  console.log("edit the notes that the user sent");
-});
+// add routes
+app.use("/",routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
