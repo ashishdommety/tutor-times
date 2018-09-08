dd85162c7f7536f8b25040e923713025a5419984
unknown <ashishuiux@gmail.com>
2018-09-07 17:12:56 -0700

    plan routes for future use

---
+++ controllers/student/index.js
@@ -0,0 +1,16 @@
+/* 
+  Decide what routes the student will be using (This could be for tutors too - ):
+    Class:
+      - schedule class - CREATE
+      - edit class - UPDATE
+      - class done - UPDATE
+      - get next class - READ
+      - cancel class - DELETE
+    Notes: 
+      - add note - CREATE
+      - edit note - UPDATE
+      - delete note - DELETE
+      - get all notes - READ
+    Associates: 
+      - view tutors/students - READ
+*/
