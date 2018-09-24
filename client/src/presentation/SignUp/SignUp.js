b5c706ab4c6660744a2dfc32c79c7411c1b40fa9
unknown <ashishuiux@gmail.com>
2018-09-24 11:41:26 -0700

    get form data

--- client/src/presentation/SignUp/SignUp.js
+++ client/src/presentation/SignUp/SignUp.js
@@ -36,11 +36,16 @@ class SignUp extends Component{
     return(
       <div>
         <form>
-          <label>First Name: <input type="text" name="firstName" value={this.state.firstname}/></label>
-          <label>Last Name: <input type="text" name="lastName" value={this.state.lastName}/></label>
-          <label>Middle Name: <input type="text" name="middleName" value={this.state.middleName}/></label>
-          <label>Email: <input type="text" name="email" value={this.state.email}/></label>
-          <label>Phone Number: <input type="text" name="phoneNumber" value={this.state.phoneNumber}/></label>
+          <label>First Name: 
+            <input type="text" name="firstName" value={this.state.firstname} onChange={this.handleInputChange}/></label>
+          <label>Last Name: 
+            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChange}/></label>
+          <label>Middle Name: 
+            <input type="text" name="middleName" value={this.state.middleName} onChange={this.handleInputChange}/></label>
+          <label>Email: 
+            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange}/></label>
+          <label>Phone Number: 
+            <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleInputChange}/></label>
           <label>I am here to 
             <select>
               <option>study</option>
@@ -48,12 +53,12 @@ class SignUp extends Component{
             </select>
           </label>
           <label>What do you hope to achieve from your classes? </label>
-          <textarea type="text" name="goal" value={this.state.goal}/>
+          <textarea type="text" name="goal" value={this.state.goal} onChange={this.handleInputChange}/>
           <label>What's your motto? </label>
-          <textarea type="text" name="motto" value={this.state.motto}/>
-          <label>What else are you interested in? </label>
-          <textarea type="text" name="bio" value={this.state.bio}/>
-          <button> Submit </button>
+          <textarea type="text" name="motto" value={this.state.motto} onChange={this.handleInputChange}/>
+          <label>Tell us a little bit about yourself: </label>
+          <textarea type="text" name="bio" value={this.state.bio} onChange={this.handleInputChange}/>
+          <button onClick={this.submit}>Submit</button>
         </form>
       </div>
     )
