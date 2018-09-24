b4b51cac63c0980bb060919aa408462739304c01
unknown <ashishuiux@gmail.com>
2018-09-24 12:05:44 -0700

    remove inputs from labels

--- client/src/presentation/SignUp/SignUp.js
+++ client/src/presentation/SignUp/SignUp.js
@@ -16,18 +16,21 @@ class SignUp extends Component{
         goal: '',
         motto: ''
       };
+
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange = (event) => {
     const value = event.target.value;
     const name = event.target.name;
-
+    console.log(value);
     this.setState({
       [name]: value
     })
   }
 
-  submit = e => {
+  handleSubmit = e => {
     e.preventDefault();
     console.log(this.state);
   }
@@ -36,29 +39,28 @@ class SignUp extends Component{
     return(
       <div>
         <form>
-          <label>First Name: 
-            <input type="text" name="firstName" value={this.state.firstname} onChange={this.handleInputChange}/></label>
-          <label>Last Name: 
-            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChange}/></label>
-          <label>Middle Name: 
-            <input type="text" name="middleName" value={this.state.middleName} onChange={this.handleInputChange}/></label>
-          <label>Email: 
-            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange}/></label>
-          <label>Phone Number: 
-            <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleInputChange}/></label>
-          <label>I am here to 
-            <select>
-              <option>study</option>
-              <option>teach</option>
-            </select>
-          </label>
+          <label>First Name: </label>
+            <input type="text" name="firstName" value={this.state.firstname} onChange={this.handleInputChange}/>
+          <label>Last Name: </label>
+            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChange}/>
+          <label>Middle Name: </label>
+            <input type="text" name="middleName" value={this.state.middleName} onChange={this.handleInputChange}/>
+          <label>Email: </label>
+            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange}/>
+          <label>Phone Number: </label>
+            <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleInputChange}/>
+          <label>I am here to:  </label>
+            Study <input type="radio" value={false} name="isTutor" onChange={this.handleInputChange}/>
+            Teach <input type="radio" value={true} name="isTutor" onChange={this.handleInputChange}/>
+         
           <label>What do you hope to achieve from your classes? </label>
           <textarea type="text" name="goal" value={this.state.goal} onChange={this.handleInputChange}/>
           <label>What's your motto? </label>
           <textarea type="text" name="motto" value={this.state.motto} onChange={this.handleInputChange}/>
           <label>Tell us a little bit about yourself: </label>
           <textarea type="text" name="bio" value={this.state.bio} onChange={this.handleInputChange}/>
-          <button onClick={this.submit}>Submit</button>
+          <br/>
+          <button onClick={this.handleSubmit}>Submit</button>
         </form>
       </div>
     )
