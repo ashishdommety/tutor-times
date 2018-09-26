da96432db31d5d6aa7c6c720e1ed72d5fdeddffb
unknown <ashishuiux@gmail.com>
2018-09-26 15:08:15 -0700

    trying to fix GoogleLogout

--- client/src/presentation/Dashboard/Navigation/Navigation.js
+++ client/src/presentation/Dashboard/Navigation/Navigation.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
-// import { GoogleLogout } from 'react-google-login'; // test GoogleLogout on Heroku
+import { GoogleLogout } from 'react-google-login';
 
 const Navigation = (props) => {
-  const logout = (response) => {
-    console.log(response);
+  const forceMyOwnLogout = ((response) => {
+    // cookie.remove('MyGoogleID', { path: '/' });
+    // if (window.gapi) {
+    //     const auth2 = window.gapi.auth2.getAuthInstance()
+    //     if (auth2 != null) {
+    //         auth2.signOut().then(
+    //             auth2.disconnect().then(this.props.onLogoutSuccess)
+    //         )
+    //     }
+    // }
+    // this.forceUpdate();
     console.log('you logged out');
     localStorage.clear(); //clear localStorage
     window.location.pathname = "/"; // send user back to the homepage
-  }
+  })
 
   return(
     <div className="nav">
@@ -20,9 +29,13 @@ const Navigation = (props) => {
         <Link to={"/dashboard/" + props.title}>{props.title[0].toUpperCase() + props.title.substring(1,props.title.length)}</Link>
       </div>
       <div>
+      {/* <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={forceMyOwnLogout}
+      ></GoogleLogout> */}
       <button 
         id="logout-button"
-        onClick={logout}
+        onClick={forceMyOwnLogout}
       >Logout</button>
       </div>
     </div>
