45a46d47e78d8c744e79275a53e1bd3821d79fde
unknown <ashishuiux@gmail.com>
2018-10-01 10:55:44 -0700

    add quiz model

---
+++ models/Quiz.js
@@ -0,0 +1,56 @@
+module.exports = function(sequelize, DataTypes){
+ // primary id	google id	quiz-id	question #	title	grade	difficulty level	score	type	question	answer
+
+  var Quiz = sequelize.define("Quiz", {
+    google_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quiz_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    question_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    grade: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    question: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    answer: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+     type: DataTypes.DATE,
+     defaultValue: sequelize.fn('now')
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.fn('now')
+    }
+  });
+
+  return Quiz;
+}
