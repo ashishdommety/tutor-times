739d34bdea82a3947787ac62cc9b5679e549bab2
unknown <ashishuiux@gmail.com>
2018-10-05 14:02:39 -0700

    align input boxes

--- client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
+++ client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
+import './QuizQuestion.css';
 
 class QuizQuestion extends Component{
   render(){
     return(
       <div>
         <h3>{this.props.question}</h3>
-        <div>
-          <div>
-            <input type="checkbox" name="opt1" value="opt1"/>{this.props.opt1}</div>
-          <div>
-            <input type="checkbox" name="opt2" value="opt2"/>{this.props.opt2}</div>
+        <div className="optionPairs">
+          <div className="options">
+            {this.props.opt1}<input type="checkbox" name="opt1" value="opt1" className="answerInput"/>
+          </div>
+          <div className="options">
+            {this.props.opt2}<input type="checkbox" name="opt2" value="opt2" className="answerInput"/>
+          </div>
         </div>
-        <div>
-          <div>
-            <input type="checkbox" name="opt3" value="opt3"/>{this.props.opt3}</div>
-          <div>
-            <input type="checkbox" name="opt4" value="opt4"/>{this.props.opt4}
+        <div className="optionPairs">
+          <div className="options">
+            {this.props.opt3}<input type="checkbox" name="opt3" value="opt3" className="answerInput"/>
+          </div>
+          <div className="options">
+            {this.props.opt4}<input type="checkbox" name="opt4" value="opt4" className="answerInput"/>
           </div>
         </div>
       </div>
