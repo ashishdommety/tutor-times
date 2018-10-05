7547916438239007b4cca4bd492d9f495b2858f4
unknown <ashishuiux@gmail.com>
2018-10-05 10:42:27 -0700

    add questions

--- client/src/presentation/Dashboard/Quiz/QuizBegin/QuizBegin.js
+++ client/src/presentation/Dashboard/Quiz/QuizBegin/QuizBegin.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './QuizBegin.css';
 import fetchQuizQuestions from '../../../../actionCreators/async/quiz/fetchQuizQuestions';
+import QuizQuestion from './QuizQuestion/QuizQuestion';
 
 class QuizBegin extends Component {
   constructor(props,match){
@@ -19,14 +20,17 @@ class QuizBegin extends Component {
         questions: result.data
       });
     }).catch((err) => console.log(err));
-    
   }
 
   render(){
     return(
       <div id="quiz-landing">
         <h1>Welcome to the <br/>{this.state.name}<br/> Quiz</h1>
-        {/* <Route /> */}
+        {!this.state.questions.length ? "loading..." :  this.state.questions.map((x,i) =>
+        <QuizQuestion
+          key={i}
+          question={x.question}
+          />)}
       </div>
     )
   }
--- client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
+++ client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
@@ -4,11 +4,15 @@ class QuizQuestion extends Component{
   render(){
     return(
       <div>
-        <h3>Question</h3>
-        <input type="checkbox" name="opt1" value=""> </input>
-        <input type="checkbox" name="opt2" value=""> </input>
-        <input type="checkbox" name="opt3" value=""> </input>
-        <input type="checkbox" name="opt4" value=""> </input>
+        <h3>{this.props.question}</h3>
+        <div>
+          <input type="checkbox" name="opt1" value="opt1"/>
+          <input type="checkbox" name="opt2" value="opt2"/>
+        </div>
+        <div>
+          <input type="checkbox" name="opt3" value="opt3"/> 
+          <input type="checkbox" name="opt4" value="opt4"/>
+        </div>
       </div>
     )
   }
