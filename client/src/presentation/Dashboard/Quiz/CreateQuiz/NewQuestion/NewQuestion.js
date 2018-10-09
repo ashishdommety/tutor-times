1792567b5ea0f15138e4418681e869c56c38143d
unknown <ashishuiux@gmail.com>
2018-10-09 16:08:18 -0700

    fix indentation

--- client/src/presentation/Dashboard/Quiz/CreateQuiz/NewQuestion/NewQuestion.js
+++ client/src/presentation/Dashboard/Quiz/CreateQuiz/NewQuestion/NewQuestion.js
@@ -7,15 +7,17 @@ class NewQuestion extends Component{
 
     let arr = [];
     for(let i=0; i<len; i++){
-      arr.push(<div className="newQuestionForm" key={i}>
-      <h3>Question {i+1}</h3>
+      arr.push(
+      <div className="newQuestionForm" key={i}>
+        <h3>Question {i+1}</h3>
         <input placeholder="question" id={"q" + i} className="questionInput"/>
         <input placeholder="answer" className="questionInput"/>
         <input placeholder="opt1" className="questionInput"/>
         <input placeholder="opt2" className="questionInput"/>
         <input placeholder="opt3" className="questionInput"/>
         <input placeholder="opt4" className="questionInput"/>
-      </div>)
+      </div>
+      )
     }
 
     return arr;
