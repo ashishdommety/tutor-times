00950a2651750503f4b7c5346ebe890d23af75be
unknown <ashishuiux@gmail.com>
2018-10-12 16:41:18 -0700

    handle next question for form input

--- client/src/presentation/Dashboard/Quiz/CreateQuiz/NewQuestion/NewQuestion.js
+++ client/src/presentation/Dashboard/Quiz/CreateQuiz/NewQuestion/NewQuestion.js
@@ -7,7 +7,7 @@ class NewQuestion extends Component{
     super(props, refs);
     this.state = {
       google_id: localStorage.google_id,
-      quiz_id: "4567", // how do we make this flexible and so that it never overwrites each other?
+      quiz_id: "123456", // how do we make this flexible and so that it never overwrites each other?
       /* Perhaps set them to numbers, a.k.a 1,2,3....
       To update this every time we create a quiz, we fetch the last number and add 1 to it to create the next quiz. But what happens if two users start creating a quiz at the same time? 
       Should we even have quiz ids? What if instead, we make sure the title can't be repeated. So, we fetch all the titles, remove duplicates, and make sure nothing is repeated, and that way, the title of the quiz is it's unique factor.  */
@@ -35,10 +35,17 @@ class NewQuestion extends Component{
     // send ajax request
     createQuestion(this.state)
       .then(result => {
-        console.log(result.data);
+        // console.log(result.data);
         this.props.incrementQuestionNumber();
         this.setState({
-          question_number: this.props.count
+          question_number: this.props.count,
+          score: "",
+          question: "",
+          answer: "",
+          opt1: "",
+          opt2: "",
+          opt3: "",
+          opt4: ""
         });
         console.log("written to db");
       }).catch(err => console.log(err));
@@ -65,7 +72,7 @@ class NewQuestion extends Component{
             <input placeholder="answer" className="questionInput" ref="qInput" name="answer" value={this.state.answer} onChange={this.handleInputChange}/>
             <input placeholder="score" className="questionInput" ref="qInput" name="score" value={this.state.score} onChange={this.handleInputChange}/>
             <input placeholder="opt1" className="questionInput" ref="qInput" name="opt1" value={this.state.opt1} onChange={this.handleInputChange}/>
-            <input placeholder="opt2" className="questionInput" ref="qInput" name="opt2" valuue={this.state.opt2} onChange={this.handleInputChange}/>
+            <input placeholder="opt2" className="questionInput" ref="qInput" name="opt2" value={this.state.opt2} onChange={this.handleInputChange}/>
             <input placeholder="opt3" className="questionInput" ref="qInput" name="opt3" value={this.state.opt3} onChange={this.handleInputChange}/>
             <input placeholder="opt4" className="questionInput" ref="qInput" name="opt4" value={this.state.opt4} onChange={this.handleInputChange}/>
             <button onClick={this.submitQuestion}>Add Questions</button>
