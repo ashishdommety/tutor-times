e64776c681048e7c5692ecd97c1c885d37b16752
unknown <ashishuiux@gmail.com>
2018-10-17 14:24:12 -0700

    highlight option selected last for quiz-taker

--- client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
+++ client/src/presentation/Dashboard/Quiz/QuizBegin/QuizQuestion/QuizQuestion.js
@@ -7,7 +7,16 @@ class QuizQuestion extends Component{
   // use a callback to add that element to an array
 
   getAnswer = (e) => {
+    //turn all other options of same question to white
+    let elements = document.getElementsByClassName(e.target.className);
+
+    for(let i=0; i<elements.length; i++){
+      elements[i].style.backgroundColor = "white";
+    }
+    
+    //turn selected answer bg to green
     e.target.style.backgroundColor = "lightgreen";
+
     let responseObj = {
       qNum: this.props.question_number,
       answer: e.target.innerText
@@ -21,18 +30,18 @@ class QuizQuestion extends Component{
       <div id={this.props.id} ref={this.props.id}>
         <h3>{this.props.question_number}. {this.props.question}</h3>
         <div className="optionPairs">
-          <div className="options" onClick={this.getAnswer} >
+          <div className={`options q${this.props.question_number}-options`} data-target="hi" onClick={this.getAnswer} >
               {this.props.opt1}
           </div>
-          <div className="options" onClick={this.getAnswer} >
+          <div className={`options q${this.props.question_number}-options`}  onClick={this.getAnswer} >
             {this.props.opt2}
           </div>
         </div>
         <div className="optionPairs" onClick={this.getAnswer} >
-          <div className="options">
+          <div className={`options q${this.props.question_number}-options`} >
             {this.props.opt3}
           </div>
-          <div className="options" onClick={this.getAnswer} >
+          <div className={`options q${this.props.question_number}-options`}  onClick={this.getAnswer} >
             {this.props.opt4}
           </div>
         </div>
