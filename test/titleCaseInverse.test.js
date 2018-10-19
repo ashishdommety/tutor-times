faa36270eb0fcac7567a17ec8c1461ffed8d2aa9
unknown <ashishuiux@gmail.com>
2018-10-19 15:21:13 -0700

    add test cases for titleCaseInverse

--- test/titleCaseInverse.test.js
+++ test/titleCaseInverse.test.js
@@ -5,4 +5,10 @@ describe("Change program title to path name", function(){
   it("'Simple Path' becomes 'simple-path'", function(){
     titleCaseInverse("Simple Path").should.equal("simple-path");
   })
+  it("'Complex Path' becomes 'complex-path'", function(){
+    titleCaseInverse("Complex Path").should.equal("complex-path");
+  })
+  it("'Animals' becomes 'animals'", function(){
+    titleCaseInverse("Animals").should.equal("animals");
+  })
 })
