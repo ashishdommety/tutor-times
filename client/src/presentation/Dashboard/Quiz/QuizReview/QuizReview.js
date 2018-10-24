343e529e6ec81a7647ea3d6f2a6c6a7b4516ee0f
unknown <ashishuiux@gmail.com>
2018-10-24 15:52:23 -0700

    dump placeholder rough question content

--- client/src/presentation/Dashboard/Quiz/QuizReview/QuizReview.js
+++ client/src/presentation/Dashboard/Quiz/QuizReview/QuizReview.js
@@ -1,20 +1,42 @@
 import React, {Component} from 'react';
 import './QuizReview.css';
 import fetchQuizQuestions from '../../../../actionCreators/async/quiz/fetchQuizQuestions';
+import EditQuestion from './EditQuestion/EditQuestion';
 
 class QuizReview extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      questions: []
+    }
   }
 
   componentDidMount(){
     let titleName = this.props.match.params.name;
     console.log(titleName);
+    fetchQuizQuestions(titleName).then((result) => {
+      console.log(result.data);
+      this.setState({
+        questions: result.data
+      });
+    }).catch((err) => console.log(err));
   }
-  
+
   render(){
     return(
-      <h1>Hi</h1>
+      <div>
+        {!this.state.questions.length ? "loading..." :  this.state.questions.map((x,i) =>
+          <EditQuestion
+              key={i}
+              id={"q"+i}
+              question={x.question}
+              question_number={x.question_number}
+              opt1={x.opt1}
+              opt2={x.opt2}
+              opt3={x.opt3}
+              opt4={x.opt4}/> 
+          )}
+      </div>
     )
   }
 }
