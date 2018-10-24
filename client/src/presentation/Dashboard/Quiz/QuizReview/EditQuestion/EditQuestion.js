80afe8d7751a20f199dc341964c5222801974c1e
unknown <ashishuiux@gmail.com>
2018-10-24 16:18:25 -0700

    add comments for editing optimization

--- client/src/presentation/Dashboard/Quiz/QuizReview/EditQuestion/EditQuestion.js
+++ client/src/presentation/Dashboard/Quiz/QuizReview/EditQuestion/EditQuestion.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 import './EditQuestion.css';
 
 class EditQuestion extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      editClicked: false,
+      editingQuestion: 0
+    }
+  }
+
+  editQuestion = () => {
+    // toggle edit button to a save button
+    // allow users to uncheck an old answer and re-check a new one
+    // make options type-able
+    // Allow user to edit one question at a time
+    console.log('question number');
+  }
+
   checkOption = (option) => {
     return option === this.props.answer ? <div className="correct-option">{option} is correct</div> : option;
   };
@@ -9,6 +25,7 @@ class EditQuestion extends Component{
   getAnswer = () => {
     console.log(this.props.answer);
   }
+
   render(){
     return(
       <div id={this.props.id} ref={this.props.id} className="review-questions">
@@ -30,7 +47,7 @@ class EditQuestion extends Component{
             {this.checkOption(this.props.opt4)}
           </div>
         </div>
-        <button className="modify-question-btn edit-question-btn">Edit</button>
+        <button className="modify-question-btn edit-question-btn" onClick={this.editQuestion}>Edit</button>
       </div>
     )
   }
